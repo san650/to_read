2a83726c72cf8ae020b8b3b306cfc9e27de21ebb
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2013-05-26 14:24:44 -0300

    Validates link field and normalize it

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,9 +2,16 @@ class Resource < ActiveRecord::Base
   attr_accessible :link
   has_many :bookmarks
 
+  validates :link, :presence => true
+  before_validation :trim_link
+
   after_create :assign_link_to_everyone
 
   private
+
+  def trim_link
+    @link.strip! unless @link.nil?
+  end
 
   def assign_link_to_everyone
     User.all.each do |user|

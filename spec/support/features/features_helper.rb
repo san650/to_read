7af2a936c7b2be97e8e064d2fb72ed74198b1b5e
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2013-06-23 14:07:12 -0300

    Remove unused specs files

--- spec/support/features/features_helper.rb
+++
@@ -1,9 +0,0 @@
-def sign_in_with_john
-  user = FactoryGirl.create(:user)
-  FactoryGirl.create(:bookmark, user: user)
-
-  visit "/users/sign_in"
-  fill_in "Email", with: "john@example.org"
-  fill_in "Password", with: "password for john"
-  click_on "Sign in"
-end

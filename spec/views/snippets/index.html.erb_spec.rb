3371ec860405cb011749a2fc8c48ed6f02262498
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2013-11-03 20:27:59 -0200

    Add anchor for URL reference in the snippet list

--- spec/views/snippets/index.html.erb_spec.rb
+++ spec/views/snippets/index.html.erb_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 describe "snippets/index" do
   it "renders a list of snippets" do
+    snippet = FactoryGirl.build(:snippet, title: "Dummy title", html: "Dummy body")
+    snippet.id = 10
     @snippets = [
-      FactoryGirl.build(:snippet, title: "Dummy title", html: "Dummy body")
+      snippet
     ]
 
     render
 
     expect(response).to have_text("Dummy title")
     expect(response).to have_text("Dummy body")
+    expect(response).to match('<a name="snippet_10" href="#snippet_10">')
   end
 end

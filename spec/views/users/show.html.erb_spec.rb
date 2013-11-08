f4832a9017f4faf93af812bf4c2b3f4b717bbafa
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2013-11-07 22:04:43 -0200

    Use definition list to list resources

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,19 +1,10 @@
 require 'spec_helper'
 
 describe "users/show" do
-  before(:each) do
-    user = FactoryGirl.create(:user)
-    FactoryGirl.create(:bookmark, user: user)
-    @catalog = BookmarksCatalog.new(user).descending
-
+  it "renders the date" do
+    @catalog = { "dummy date" => [] }
     render
-  end
 
-  it "renders resource's address" do
-    expect(rendered).to match("http://www.example.org/")
-  end
-
-  it "renders archive link" do
-    expect(rendered).to match("Archive")
+    expect(rendered).to match("dummy date")
   end
 end
